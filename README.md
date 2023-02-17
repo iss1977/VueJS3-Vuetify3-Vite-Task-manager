e9b2d75072a62ad7cfb4351044eaaf0eb716c1dd
iss1977 <ioan.szucs@gmail.com>
2023-02-17 11:43:03 +0200

    starter vue3 + vuetify + vite

---
+++ README.md
@@ -0,0 +1,14 @@
+# TaskMan using VueJS 3 and Vuetify 3 with responsive layout
+
+This is a learn path starting from Danny Connell's Vuetify2 and Vue JS 2 Udemy Course.
+
+Following modifications differ from the author's starting point:
+
+| Description|Tutorial|own Code|
+| ------------- |:-------------:|:-----:|
+|Build tool|Vue CLI|**Vite**|
+|Vue JS|v2|**v3**|
+|Vuetify|v2|**v3**|
+
+---
+
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+import router from './router'
+
+
+
+const app = createApp(App)
+
+app.use(vuetify)
+
+app.use(router)
+
+app.mount('#app')
