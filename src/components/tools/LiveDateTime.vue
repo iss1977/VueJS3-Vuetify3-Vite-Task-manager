70f1fe2c48aa5ae3c1fe21684f6b816235675e6b
iss1977 <ioan.szucs@gmail.com>
2023-03-08 01:56:24 +0200

    add live date time

---
+++ src/components/tools/LiveDateTime.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-teal-lighten-4 text-body-2 font-weight-medium pa-1">
+    {{ date }}
+  </div>
+</template>
+
+<script setup>
+import { computed, onUnmounted, ref } from 'vue';
+import { format } from 'date-fns';
+
+const date = ref(null)
+
+const getDate = () => format( Date.now(), 'MMMM d, H:mm:ss')
+
+const timer = setInterval( () => {
+  date.value = getDate()
+}, 1000);
+
+onUnmounted(() => {
+  clearInterval(timer);
+});
+</script>
