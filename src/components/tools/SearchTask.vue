20459bfbe39d8b32db9830eb5468adf142513fe4
iss1977 <ioan.szucs@gmail.com>
2023-03-07 20:11:15 +0200

    add search task functionality

---
+++ src/components/tools/SearchTask.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-text-field
+    :modelValue="$store.searchInput"
+    v-on:update:modelValue="$store.dispatch('updateSearchTerm', $event)"
+    prepend-inner-icon="mdi-magnify"
+    label="search for a task"
+    variant="underlined"
+    density="compact"
+    clearable
+    hide-details
+  ></v-text-field>
+</template>
+
+<script setup>
+
+
+</script>
